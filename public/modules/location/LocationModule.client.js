3c525b4fd4687ec5e6c55fad03e50dc21e954886
John Espenhahn <john@espenhahn.org>
2015-06-09 10:04:08 -0400

    Update environment for production

--- public/modules/location/LocationModule.client.js
+++ public/modules/location/LocationModule.client.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('location', ['uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {
+angular.module('location', ['uiGmapgoogle-maps']).config([ 'uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyD0d7h9MKnvO8J_aWUO1PdJP4hntSzRWfA',
         v: '3.17',
         libraries: 'weather,geometry,visualization'
     });
-});
+}]);
--- public/modules/location/controllers/LocationController.client.js
+++ public/modules/location/controllers/LocationController.client.js
@@ -1,34 +1,36 @@
 'use strict';
 
-angular.module('location').controller('LocationController', function($http, $window, $scope, uiGmapGoogleMapApi) {
-	$scope.hasGeolocation = function() {
-		return $window.navigator.geolocation;
-	};
-	
-	var geolocateError = false, lat, lng;
-	if ($scope.hasGeolocation()) {
-		$window.navigator.geolocation.getCurrentPosition(
-			function(position) { // success
-				lat = position.coords.latitude;
-				lng = position.coords.longitude;
-			},
-			function() { // error
-				geolocateError = true;
-			}
-		);
-	}
-	
-	uiGmapGoogleMapApi.then(function(maps) {
-		// TODO
-		if ($scope.control.type === 'Location') {
-			console.log('Google maps ready for main location view');
-		} else if ($scope.control.type === 'LocationSetup') {
-			console.log('Google maps ready for setup location view');
-		} else {
-			console.log('Google maps ready for other location view');
+angular.module('location').controller('LocationController', [ '$http', '$window', '$scope', 'uiGmapGoogleMapApi',
+	function($http, $window, $scope, uiGmapGoogleMapApi) {
+		$scope.hasGeolocation = function() {
+			return $window.navigator.geolocation;
+		};
+		
+		var geolocateError = false, lat, lng;
+		if ($scope.hasGeolocation()) {
+			$window.navigator.geolocation.getCurrentPosition(
+				function(position) { // success
+					lat = position.coords.latitude;
+					lng = position.coords.longitude;
+				},
+				function() { // error
+					geolocateError = true;
+				}
+			);
 		}
-    });
-})
+		
+		uiGmapGoogleMapApi.then(function(maps) {
+			// TODO
+			if ($scope.control.type === 'Location') {
+				console.log('Google maps ready for main location view');
+			} else if ($scope.control.type === 'LocationSetup') {
+				console.log('Google maps ready for setup location view');
+			} else {
+				console.log('Google maps ready for other location view');
+			}
+	    });
+	}
+])
 
 .directive('autoAddress', function() {
 	return {
