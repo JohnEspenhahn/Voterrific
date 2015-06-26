6aef8b1a4a2ef023ad6b6a89bd3fec0382ce6000
John Espenhahn <john@espenhahn.org>
2015-06-26 18:16:16 -0400

    Reorder config

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	passport = require('passport'),
 	session = require('express-session'),
+	compression = require('compression'),
+	cookieParser = require('cookie-parser'),
+	helmet = require('helmet'),
 	MongoStore = require('connect-mongo')(session),
 	ejs = require('ejs'),
 	flash = require('flash'),
@@ -52,9 +55,33 @@ winston.handleExceptions(new winston.transports.File({
 
 // Setup app
 app.set('view engine', 'ejs');
+
+// Should be placed before express.static
+app.use(compression({
+	filter: function(req, res) {
+		return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+	},
+	level: 3
+}));
+
+// Environment dependent middleware
+if (process.env.NODE_ENV === 'production') {
+	app.locals.cache = 'memory';
+} else {
+	// Disable views cache
+	app.set('view cache', false);
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+// Use helmet to secure Express headers
+app.use(helmet.xframe());
+app.use(helmet.xssFilter());
+app.use(helmet.nosniff());
+app.use(helmet.ienoopen());
+app.disable('x-powered-by');
 
 // Express MongoDB session storage
 app.use(session({
@@ -68,6 +95,13 @@ app.use(session({
 app.use(express.static(path.resolve('./public')));
 app.use(express.static(path.resolve('./bower_components')));
 
+// CookieParser should be above session
+app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // Load models
 var modelFiles = glob.sync('./app/models/**/*.js');
 for (var i = 0; i < modelFiles.length; i++) { require(modelFiles[i]); }
@@ -75,11 +109,6 @@ for (var i = 0; i < modelFiles.length; i++) { require(modelFiles[i]); }
 // Load routers	
 var routeFiles = glob.sync('./app/routes/**/*.js');
 for (var i = 0; i < routeFiles.length; i++) { require(routeFiles[i])(app); }
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash()); // use connect-flash for flash messages stored in session
-
 
 // Single index file route
 app.get('*', function(req, res) {
